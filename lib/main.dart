87bf2bf30650ea34c51fe3d3454a22d4957827c1
Toretay <abdymvzhit@gmail.com>
2025-06-03 13:58:13 +0500

    initialized firebase and fixed build.gradle error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:real_time_chat_app/firebase_options.dart';
 import 'package:real_time_chat_app/pages/welcome_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.android);
   runApp(const MyApp());
 }
 
