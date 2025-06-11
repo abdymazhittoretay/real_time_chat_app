39e44e2c154ce3084dd7a22768c31c2f2ab57bbc
Toretay <abdymvzhit@gmail.com>
2025-06-11 16:08:44 +0500

    created getUsersList stream method

--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -6,44 +6,13 @@ final ValueNotifier<FirestoreService> firestoreService = ValueNotifier(
 );
 
 class FirestoreService {
-  // final CollectionReference _posts = FirebaseFirestore.instance.collection(
-  //   "posts",
-  // );
+  final FirebaseFirestore _instance = FirebaseFirestore.instance;
 
-  // Future<void> addPost(PostModel post) {
-  //   return _posts.add({
-  //     "email": post.email,
-  //     "username": post.username,
-  //     "content": post.content,
-  //     "likes": post.likes,
-  //     "timestamp": post.timestamp,
-  //     "whoLiked": post.whoLiked,
-  //   });
-  // }
-
-  // Stream<QuerySnapshot> getPosts() {
-  //   final postsSnapshot =
-  //       _posts.orderBy("timestamp", descending: true).snapshots();
-  //   return postsSnapshot;
-  // }
-
-  // Future<void> removePost(String docID) {
-  //   return _posts.doc(docID).delete();
-  // }
-
-  // Future<void> addLike(String docID) async {
-  //   return _posts.doc(docID).update({
-  //     "likes": FieldValue.increment(1),
-  //     "whoLiked": FieldValue.arrayUnion([authService.value.currentUser!.email]),
-  //   });
-  // }
-
-  // Future<void> removeLike(String docID) async {
-  //   return _posts.doc(docID).update({
-  //     "likes": FieldValue.increment(-1),
-  //     "whoLiked": FieldValue.arrayRemove([
-  //       authService.value.currentUser!.email,
-  //     ]),
-  //   });
-  // }
+  Stream<List<Map<String, dynamic>>> getUsersList() {
+    return _instance.collection("users").snapshots().map((snapshot) {
+      return snapshot.docs.map((doc) {
+        return doc.data();
+      }).toList();
+    });
+  }
 }
