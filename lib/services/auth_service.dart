7b3320a7bdb7f5206495943b9e2a3a1a40c439e7
Toretay <abdymvzhit@gmail.com>
2025-06-11 16:13:21 +0500

    added users emails to the database when they r | s

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/widgets.dart';
 
@@ -5,6 +6,7 @@ final ValueNotifier<AuthService> authService = ValueNotifier(AuthService());
 
 class AuthService {
   final _instance = FirebaseAuth.instance;
+  final _firestoreInstance = FirebaseFirestore.instance;
 
   User? get currentUser => _instance.currentUser;
 
@@ -15,10 +17,15 @@ class AuthService {
     required String password,
     required String username,
   }) async {
+    _firestoreInstance.collection("users").doc(currentUser!.uid).set({
+      "uid": currentUser!.uid,
+      "email": email,
+    });
     await _instance.createUserWithEmailAndPassword(
       email: email,
       password: password,
     );
+
     await currentUser!.updateDisplayName(username);
   }
 
@@ -26,6 +33,10 @@ class AuthService {
     required String email,
     required String password,
   }) async {
+    _firestoreInstance.collection("users").doc(currentUser!.uid).set({
+      "uid": currentUser!.uid,
+      "email": email,
+    });
     await _instance.signInWithEmailAndPassword(
       email: email,
       password: password,
