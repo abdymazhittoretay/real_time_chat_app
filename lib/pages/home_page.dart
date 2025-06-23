07b5d82b69ac25b94488cf2f9a577d794f4c6750
Toretay <abdymvzhit@gmail.com>
2025-06-23 20:39:08 +0500

    added setstate after navigator pops

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,9 +3,14 @@ import 'package:real_time_chat_app/pages/chat_page/chat_page.dart';
 import 'package:real_time_chat_app/services/auth_service.dart';
 import 'package:real_time_chat_app/services/firestore_service.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,7 +59,9 @@ class HomePage extends StatelessWidget {
                             receiverID: user["uid"],
                           ),
                         ),
-                      );
+                      ).then((_) {
+                        setState(() {});
+                      });
                     },
                     child: ListTile(
                       title: Text(user["email"]),
